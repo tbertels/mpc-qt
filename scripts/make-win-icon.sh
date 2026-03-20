e0fa68afa2afb2bace87beb09bd510137271fb50
Thomas Bertels <3265870+tbertels@users.noreply.github.com>
2026-03-20 15:28:50 +0100

    scripts: Check if Windows icon has actually been written

--- scripts/make-win-icon.sh
+++ scripts/make-win-icon.sh
@@ -4,4 +4,9 @@ mkdir build
 if [ ! -f $ICOFILE ]; then
     magick res/images/icon/mpc-qt.svg -define icon:auto-resize=16,24,32,48,64,72,96,128,256 -antialias -transparent white -compress zip $ICOFILE
 fi
-echo "$ICOFILE written"
+if [ -f $ICOFILE ]; then
+    echo "$ICOFILE written"
+else
+    echo "$ICOFILE NOT written"
+    exit 1
+fi
