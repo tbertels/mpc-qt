7717079d500066171bc939e03b43f29a74967da2
Thomas Bertels <3265870+tbertels@users.noreply.github.com>
2026-03-20 15:49:19 +0100

    scripts: Fix paths for Windows icon

--- scripts/make-win-icon.sh
+++ scripts/make-win-icon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-ICOFILE="build/mpc-qt.ico"
-mkdir build
+ICOFILE="../build/mpc-qt.ico"
+mkdir ../build
 if [ ! -f $ICOFILE ]; then
-    magick res/images/icon/mpc-qt.svg -define icon:auto-resize=16,24,32,48,64,72,96,128,256 -antialias -transparent white -compress zip $ICOFILE
+    magick ../res/images/icon/mpc-qt.svg -define icon:auto-resize=16,24,32,48,64,72,96,128,256 -antialias -transparent white -compress zip $ICOFILE
 fi
 if [ -f $ICOFILE ]; then
     echo "$ICOFILE written"
