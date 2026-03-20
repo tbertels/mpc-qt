a9e45bf146d31ed84c72b8cd5890f24321738803
Thomas Bertels <3265870+tbertels@users.noreply.github.com>
2026-03-20 17:33:42 +0100

    scripts: Make the Windows icon script update the icon in the source dir

--- scripts/make-win-icon.sh
+++ scripts/make-win-icon.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-echo $(magick --version)
-ICOFILE="../build/mpc-qt.ico"
-mkdir ../build
-if [ ! -f $ICOFILE ]; then
-    magick ../res/images/icon/mpc-qt.svg -define icon:auto-resize=16,24,32,48,64,72,96,128,256 -antialias -transparent white -compress zip $ICOFILE
-fi
-if [ -f $ICOFILE ]; then
-    echo "$ICOFILE written"
-else
-    echo "$ICOFILE NOT written"
-    exit 1
-fi
+ICONPATH="../res/images/icon"
+    magick $ICONPATH/mpc-qt.svg -define icon:auto-resize=16,24,32,48,64,72,96,128,256 \
+        -antialias -transparent white -compress zip $ICONPATH/mpc-qt.ico
